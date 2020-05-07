af0e914ecc4f7eb9866dfd0795b5f434c45cf04a
pobsteta <pascal.obstetar@gmail.com>
2020-05-07 08:30:52 +0200

    initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,63 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+golem::fill_desc(
+  pkg_name = "shinyCNES", # The Name of the package containing the App 
+  pkg_title = "shinyCNES", # The Title of the package containing the App 
+  pkg_description = "This package download Sentinel 2 image on THEIA platform and preprocess this to use in calculation", # The Description of the package containing the App 
+  author_first_name = "Pascal", # Your First Name
+  author_last_name = "Obstetar", # Your Last Name
+  author_email = "pascal.obstetar@gmail.com", # Your Email
+  repo_url = "https://github.com/pobsteta/shinyCNES.git" # The URL of the GitHub Repo (optional) 
+)     
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_gpl3_license( name = "Pascal Obstetar" )
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+
+## If you have data in your package
+usethis::use_data_raw( name = "data-test", open = FALSE )
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::remove_favicon()
+golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
